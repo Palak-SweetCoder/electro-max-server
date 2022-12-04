f64c2f18570cec17357b94aaef4e4eb03acd40f7
Palak-SweetCoder <pspalak6@gmail.com>
2022-12-04 15:30:53 +0600

    server anh mongodb connection done with .env

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middlewares
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efiqtg7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+    const collection = client.db('test').collection('devices');
+    console.log('electro max server connected with db successfully!!!');
+    // perform actions on the collection object
+    client.close();
+});
 
 // root
 app.get('/', (req, res) => {
